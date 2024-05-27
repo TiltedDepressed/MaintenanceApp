2bc78ed21381b9ef2f76555d205befdf5f3315c5
TiltedDepressed <tosltikov52@mail.ru>
2024-05-27 19:46:21 +0500

    авторизация регистрация

--- app/src/main/java/com/example/maintenanceapplication/activities/LoginActivity.kt
+++ app/src/main/java/com/example/maintenanceapplication/activities/LoginActivity.kt
@@ -1,21 +1,90 @@
 package com.example.maintenanceapplication.activities
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.maintenanceapplication.R
 import com.example.maintenanceapplication.databinding.ActivityLoginBinding
+import com.example.maintenanceapplication.datasource.ServiceBuilder
+import com.example.maintenanceapplication.interfaces.Api
+import com.example.maintenanceapplication.model.User
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var login: String
+
+    private lateinit var password: String
+
+    private lateinit var sharePreference: SharedPreferences
+
+    private var rememberMe: Boolean? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.backButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity,RegActivity::class.java)
+            startActivity(intent)
+        }
+
+      binding.continueButton.setOnClickListener {
+          login = binding.loginEditText.text.toString()
+          password = binding.passwordEditText.text.toString()
+          if(login.isNotEmpty() && password.isNotEmpty()){
+              signIn(login,password)
+          }
+      }
+
+
+    }
+
+    private fun signIn(login: String, password: String) {
+
+        sharePreference = getSharedPreferences("MY_PRE", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharePreference.edit()
+
+
+        val data = JsonObject()
+        data.addProperty("login",login)
+        data.addProperty("password",password)
+        val api = ServiceBuilder.buildService(Api::class.java)
+        api.signIn(data).enqueue(object: Callback<User>{
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                 if(response.isSuccessful){
+                     rememberMe = true
+                     response.body()?.let { user->
+                         editor.putString("USER_ID",user.userId)
+                         editor.putString("TOKEN", user.token)
+                         editor.putString("ROLE",user.role)
+                         editor.putBoolean("REMEMBER_ME",rememberMe!!)
+                     }
+                     editor.apply()
+
+                     val intent = Intent(this@LoginActivity,PinActivity::class.java)
+                     startActivity(intent)
+                 }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Log.e("RegActivity",t.message.toString())
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/maintenanceapplication/activities/RegActivity.kt
+++ app/src/main/java/com/example/maintenanceapplication/activities/RegActivity.kt
@@ -1,15 +1,32 @@
 package com.example.maintenanceapplication.activities
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.maintenanceapplication.R
 import com.example.maintenanceapplication.databinding.ActivityRegBinding
+import com.example.maintenanceapplication.datasource.ServiceBuilder
+import com.example.maintenanceapplication.interfaces.Api
+import com.example.maintenanceapplication.model.User
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RegActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityRegBinding
+     private lateinit var binding: ActivityRegBinding
+     private lateinit var  login: String
+     private lateinit var password: String
+     private lateinit var email: String
+     private lateinit var sharePreference: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +34,62 @@ class RegActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.continueButton.setOnClickListener{
+
+            login =  binding.loginEditText.text.toString()
+            password = binding.passwordEditText.text.toString()
+            email = binding.emailEditText.text.toString()
+
+            if(login.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty()){
+                createAccount(login,password,email)
+            }
+        }
+
+        autoLogIn()
+
+
+    }
+
+    private fun autoLogIn() {
+        sharePreference = getSharedPreferences("MY_PRE", Context.MODE_PRIVATE)
+        if(sharePreference.getBoolean("REMEMBER_ME", false)){
+            val intent = Intent(this@RegActivity, PinActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun createAccount(login: String, password: String, email: String) {
+
+        sharePreference = getSharedPreferences("MY_PRE", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharePreference.edit()
+
+        val data = JsonObject()
+        data.addProperty("login",login)
+        data.addProperty("password",password)
+        data.addProperty("email",email)
+        val api = ServiceBuilder.buildService(Api::class.java)
+        api.signUp(data).enqueue(object: Callback<User>{
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if(response.isSuccessful){
+                        response.body().let { user ->
+                            editor.putString("login",login)
+                            editor.putString("password",password)
+                            editor.putString("email",email)
+
+                      editor.apply()
+                    }
+
+                    val intent = Intent(this@RegActivity,LoginActivity::class.java)
+                    startActivity(intent)
+                }
+                if(response.code() == 409){
+                    Toast.makeText(applicationContext, "User already exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Log.e("RegActivity",t.message.toString())
+            }
+
+        })
     }
 }
