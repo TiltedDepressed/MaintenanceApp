84e17ad8dca53a7a548e334e8fe5c8d5805642e9
TiltedDepressed <tosltikov52@mail.ru>
2024-05-29 19:52:37 +0500

    функц

--- app/src/main/java/com/example/maintenanceapplication/datasource/ServiceBuilder.kt
+++ app/src/main/java/com/example/maintenanceapplication/datasource/ServiceBuilder.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://342d-88-85-223-172.ngrok-free.app/")
+        .baseUrl("https://0cb3-88-85-223-172.ngrok-free.app/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     fun <T> buildService(service: Class<T>): T {
